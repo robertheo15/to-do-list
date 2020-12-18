fce2deb8c9e5a77cbdd32a61b2970c651ca6d9b7
robertheo <robertheo.rt@gmail.com>
2020-12-18 22:59:05 +0700

    ok

--- Laravel8/resources/views/home.blade.php
+++ Laravel8/resources/views/home.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.carousell')
+
 <div class="container">
-    <div class="row justify-content-center">
-        @include('layouts.product')
-    </div>
+    
 </div>
 @endsection
