8ab0710edec40f53feba5c616140f9e79040cfd2
robertheo <robertheo.rt@gmail.com>
2020-12-19 22:15:47 +0700

    update content home

--- todo/resources/views/home.blade.php
+++ todo/resources/views/home.blade.php
@@ -2,10 +2,37 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-      <h1>hi</h1>
-      
-    </div>
+<div class="container text-center">
+  
+      <div class="row justify-content-center">
+        <h1>To-do-list</h1>
+        <div class="card text-center bg-dark">
+          <div class="card-header">
+            <h3>My list</h3> 
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Add task</h5>
+            <form>
+              <div class="mb-3">
+                <label for="task" class="form-label">New task</label>
+                <input type="text" class="form-control" id="task" >
+              </div>
+              <button type="submit" class="btn btn-primary">Add</button>
+            </form>
+          </div>
+          <div class="card-body">
+            <p>Empty</p>
+            <div class="form-check form-switch">
+              <p>Empty</p>
+              <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+              <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+              
+            </div>
+          </div>
+          
+        </div>
+     
+  </div>
+    
 </div>
 @endsection
