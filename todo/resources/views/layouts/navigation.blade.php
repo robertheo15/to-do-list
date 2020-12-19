71526c957830c48d8005b1da9219cfa2984bc291
robertheo <robertheo.rt@gmail.com>
2020-12-19 22:16:10 +0700

    update nav

--- todo/resources/views/layouts/navigation.blade.php
+++ todo/resources/views/layouts/navigation.blade.php
@@ -1,1 +1,7 @@
-<nav>navigation</nav>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">To do list</a>
+  </div>
+</div>
+</nav>
